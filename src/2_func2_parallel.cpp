5625f11f8d571c6f05ce1b435d8ce36b88d8ff51
Heather Zhou <heatherjzhou@ucla.edu>
2020-03-30 15:40:44 -0700

    Complete RcppParallel code that is reproducible

--- src/2_func2_parallel.cpp
+++ src/2_func2_parallel.cpp
@@ -1,31 +1,39 @@
-#include <RcppArmadilloExtensions/sample.h>
+#include <RcppArmadillo.h>
 #include <RcppParallel.h>
 using namespace arma;
 
 #include <convert_seed.h> //For dqrng::convert_seed
 #include <R_randgen.h> //For dqrng::R_random_int
+#include <dqrng_distribution.h> //For dqrng::rng64_t, dqrng::generator, dqrng::xoroshiro128plus, and dqrng::normal_distribution
 
-vec generateVector(const int m){
-  mat noiseMatrix=randn(m,2);
+vec generateVector(const int n,dqrng::rng64_t rng){
+  arma::mat noiseMatrix(n,2);
+  //Draw each entry from normal(0,1)
+  dqrng::normal_distribution normal(0,1);
+  noiseMatrix.imbue([&](){return normal(*rng);});
 
-  vec sampleSpace=linspace(1,4,4); //Vector of length 4, the values being 1 through 4
-  vec noiseVector=Rcpp::RcppArmadillo::sample(sampleSpace,m,true);
+  arma::vec noiseVector(n);
+  //Draw each entry from (1,2,3,4) with replacement (equal probabilities)
+  noiseVector.imbue([&](){return (*rng)(uint32_t(4))+1;});
+  //cout<<noiseVector<<endl; //4.0000, 3.0000, 1.0000, 2.0000, 2.0000, ...
 
-  vec toReturn=noiseMatrix.col(0)+noiseMatrix.col(1)+noiseVector;
+  arma::vec toReturn=noiseMatrix.col(0)+noiseMatrix.col(1)+noiseVector;
   return toReturn;
 }
 
 struct RandomFill:public RcppParallel::Worker{
   RcppParallel::RMatrix<double> outputRM;
   const int m;
-  uint64_t seed;
+  const uint64_t seed;
 
   RandomFill(Rcpp::NumericMatrix outputRcpp,const uint64_t seed)
     :outputRM(outputRcpp),m(outputRcpp.nrow()),seed(seed){};
 
   void operator()(std::size_t begin,std::size_t end){
+    dqrng::rng64_t rng=dqrng::generator<dqrng::xoroshiro128plus>(seed);
+    rng->seed(seed,end);
     for (std::size_t colIndex=begin;colIndex<end;colIndex++) {
-      vec newColumn=generateVector(m);
+      vec newColumn=generateVector(m,rng);
       for (int obsIndex=0;obsIndex<m;obsIndex++){ //Fill the column in output.
         outputRM(obsIndex,colIndex)=newColumn(obsIndex);
       }
@@ -34,12 +42,12 @@ struct RandomFill:public RcppParallel::Worker{
 };
 
 //[[Rcpp::export]]
-Rcpp::NumericMatrix parallel_random_matrix(const int m,const int n,Rcpp::Nullable<Rcpp::IntegerVector> seed=R_NilValue){
+Rcpp::NumericMatrix parallel_random_matrix(const int m,const int n,Rcpp::Nullable<Rcpp::IntegerVector> seed){
   uint64_t _seed;
   if (seed.isNotNull()){
-    _seed=dqrng::convert_seed<uint64_t>(seed.as());
+    _seed=dqrng::convert_seed<uint64_t>(seed.as()); //This stores the value of seed in _seed. Simply using "_seed=seed;" doesn't work.
   }
-  else{ //Get a seed from R's RNG in case the user did not provide one
+  else{ //Generate a seed if the user did not provide one
     _seed=dqrng::convert_seed<uint64_t>(Rcpp::IntegerVector(2,dqrng::R_random_int));
   }
 
--- test/test1.R
+++ test/test1.R
@@ -1,10 +1,13 @@
 library(testPackage)
 
-res1<-testFunction(m=7,n=3,seed=5)
+res1<-testFunction(m=7,n=3,seed=10)
 print(res1)
-res2<-testFunction(m=7,n=3,seed=5)
+res2<-testFunction(m=7,n=3,seed=10)
 print(res2)
 print(res1==res2)
 
 res1<-testFunction(m=7,n=3,seed=5)
+print(res1)
 res1<-testFunction(m=7,n=3,seed=NULL)
+print(res1)
+
